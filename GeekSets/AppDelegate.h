e4deda289718e9eb198c82723cc79cec8cdebe77
Abhijeet Mishra <abhijeet@pharmeasy.in>
2016-10-13 00:56:52 +0530

    Initial Commit

---
+++ GeekSets/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GeekSets
+//
+//  Created by Abhijeet Mishra on 13/10/16.
+//  Copyright © 2016 Abhijeet Mishra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GeekSets/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GeekSets
+//
+//  Created by Abhijeet Mishra on 13/10/16.
+//  Copyright © 2016 Abhijeet Mishra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
